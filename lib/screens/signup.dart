82894046cc7e830b16bc856d58b4252f8f5f2ca9
catsnax <107341849+catsnax@users.noreply.github.com>
2024-09-25 00:36:07 +0800

    feat: created home page, social icons widgets for login and signup files

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
@@ -7,4 +8,32 @@ class SignupPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Placeholder();
   }
+}
+
+class SocialIconsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        buildIcon(FontAwesomeIcons.facebook),
+        SizedBox(width: 10),
+        buildIcon(FontAwesomeIcons.google),
+        SizedBox(width: 10),
+        buildIcon(
+          FontAwesomeIcons.apple,
+        ),
+      ],
+    );
+  }
+}
+
+Widget buildIcon(IconData iconData) {
+  return RawMaterialButton(
+    onPressed: () {},
+    padding: EdgeInsets.fromLTRB(0, 11, 0, 11),
+    fillColor: Color.fromRGBO(236, 236, 236, 1),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    child: Icon(iconData, color: Colors.black),
+  );
 }
