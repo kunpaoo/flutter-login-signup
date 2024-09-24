2efea5412eede42f9a6a346a1ddb268d423e8769
anya <txcokun@gmail.com>
2024-09-25 01:33:37 +0800

    feat: login page for desktop, initial logo, password censored

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 import '../social_icons.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -9,118 +10,150 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Size size = MediaQuery.of(context).size;
-    Color primary = const Color.fromRGBO(31, 65, 187, 1);
     return Scaffold(
-      body: Center(
-        child: Container(
-<<<<<<< HEAD
-          width: 400,
-          height: 500,
-          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 50),
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(10)),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-=======
-          width: 600,
-          height: 500,
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.black),
-              borderRadius: BorderRadius.circular(10)),
-          child: Column(
->>>>>>> 06c53ded3a14b4b3e11a090db67f68c247c2ced5
-            children: [
-              Text(
-                "Login here",
-                style: TextStyle(
-                    fontSize: 25, fontWeight: FontWeight.w900, color: primary),
+      body: ResponsiveBuilder(
+        builder: (context, sizingInformation) {
+          if (sizingInformation.deviceScreenType == DeviceScreenType.mobile) {
+            return MobileWidget();
+          } else {
+            return DesktopWidget();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class MobileWidget extends StatelessWidget {
+  const MobileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Color primary = const Color.fromRGBO(31, 65, 187, 1);
+    return LoginInterface(primary: primary);
+  }
+}
+
+class DesktopWidget extends StatelessWidget {
+  const DesktopWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Color primary = const Color.fromRGBO(31, 65, 187, 1);
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('assets/cat_logo.png',
+              width: 350, // Set the desired width
+              height: 350),
+          LoginInterface(primary: primary)
+        ],
+      ),
+    );
+  }
+}
+
+class LoginInterface extends StatelessWidget {
+  const LoginInterface({
+    super.key,
+    required this.primary,
+  });
+
+  final Color primary;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 400,
+        height: 600,
+        padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 50),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Login here",
+              style: TextStyle(
+                  fontSize: 25, fontWeight: FontWeight.w900, color: primary),
+            ),
+            const SizedBox(height: 20),
+            const SizedBox(
+              width: 200,
+              child: Text(
+                "Welcome back you've been missed!",
+                softWrap: true,
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
               ),
-<<<<<<< HEAD
-              const SizedBox(height: 20),
-              const SizedBox(
-                width: 200,
-                child: Text(
-                  "Welcome back you've been missed!",
-                  softWrap: true,
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
+            ),
+            const SizedBox(height: 20),
+            InputText('Username'),
+            InputText('Password', true),
+            SizedBox(
+              width: double.infinity,
+              child: Text("Forgot your Password?",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                    color: primary,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 10,
+                  )),
+            ),
+            SizedBox(height: 20),
+            MaterialButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+              color: primary,
+              minWidth: double.infinity,
+              elevation: 10,
+              padding: EdgeInsets.all(20),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)),
+              child: Text(
+                "Sign in",
+                style: TextStyle(color: Colors.white),
               ),
-              const SizedBox(height: 20),
-              InputText('Username'),
-              InputText('Password'),
-              SizedBox(
-                width: double.infinity,
-                child: Text("Forgot your Password?",
-                    textAlign: TextAlign.end,
-                    style: TextStyle(
-                      color: primary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 10,
-                    )),
-              ),
-              SizedBox(height: 20),
-              MaterialButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/home');
-                },
-                color: primary,
-                minWidth: double.infinity,
-                elevation: 10,
-                padding: EdgeInsets.all(20),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
-                child: Text(
-                  "Sign in",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              SizedBox(height: 50),
-              const Text("Socials")
-=======
-              Text("Description"),
-              Text("Username"),
-              Text("Email"),
-              Text("Submit"),
-              Text("Forgot Password"),
-              Text("Socials"),
-              SocialIconsWidget(),
->>>>>>> 06c53ded3a14b4b3e11a090db67f68c247c2ced5
-            ],
-          ),
+            ),
+            SizedBox(height: 50),
+            const Text("Socials"),
+            SocialIconsWidget(),
+          ],
         ),
       ),
     );
   }
+}
 
-  Container InputText(String label) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      child: TextFormField(
-        style: TextStyle(fontSize: 15),
-        decoration: InputDecoration(
-            contentPadding: EdgeInsets.all(15),
-            labelText: label,
-            labelStyle: TextStyle(fontSize: 13),
-            floatingLabelStyle:
-                TextStyle(color: Color.fromRGBO(31, 65, 187, 1)),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: const BorderSide(
-                width: 0,
-                style: BorderStyle.none,
-              ),
+Container InputText(String label, [bool password = false]) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 10),
+    child: TextFormField(
+      style: TextStyle(fontSize: 15),
+      obscureText: password,
+      obscuringCharacter: '*',
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(15),
+          labelText: label,
+          labelStyle: TextStyle(fontSize: 13),
+          floatingLabelStyle: TextStyle(color: Color.fromRGBO(31, 65, 187, 1)),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+              width: 0,
+              style: BorderStyle.none,
             ),
-            fillColor: const Color.fromRGBO(241, 244, 255, 1),
-            filled: true,
-            focusedBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Color.fromRGBO(31, 65, 187, 1)))),
-      ),
-    );
-  }
+          ),
+          fillColor: const Color.fromRGBO(241, 244, 255, 1),
+          filled: true,
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Color.fromRGBO(31, 65, 187, 1)))),
+    ),
+  );
 }
 
 class SocialIconsWidget extends StatelessWidget {
