d05d478b9bef2b952ad504d53baeaf38acd18087
catsnax <107341849+catsnax@users.noreply.github.com>
2024-09-25 01:01:57 +0800

    separated social icon widget into another file

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../social_icons.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
   @override
@@ -34,32 +36,4 @@ class LoginPage extends StatelessWidget {
       ),
     );
   }
-}
-
-class SocialIconsWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        buildIcon(FontAwesomeIcons.facebook),
-        SizedBox(width: 10),
-        buildIcon(FontAwesomeIcons.google),
-        SizedBox(width: 10),
-        buildIcon(
-          FontAwesomeIcons.apple,
-        ),
-      ],
-    );
-  }
-}
-
-Widget buildIcon(IconData iconData) {
-  return RawMaterialButton(
-    onPressed: () {},
-    padding: EdgeInsets.fromLTRB(0, 11, 0, 11),
-    fillColor: Color.fromRGBO(236, 236, 236, 1),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-    child: Icon(iconData, color: Colors.black),
-  );
 }
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../social_icons.dart'; // Make sure the file path is correct
+
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
 
@@ -8,32 +10,4 @@ class SignupPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Placeholder();
   }
-}
-
-class SocialIconsWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        buildIcon(FontAwesomeIcons.facebook),
-        SizedBox(width: 10),
-        buildIcon(FontAwesomeIcons.google),
-        SizedBox(width: 10),
-        buildIcon(
-          FontAwesomeIcons.apple,
-        ),
-      ],
-    );
-  }
-}
-
-Widget buildIcon(IconData iconData) {
-  return RawMaterialButton(
-    onPressed: () {},
-    padding: EdgeInsets.fromLTRB(0, 11, 0, 11),
-    fillColor: Color.fromRGBO(236, 236, 236, 1),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-    child: Icon(iconData, color: Colors.black),
-  );
 }
