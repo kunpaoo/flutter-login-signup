fdc7523f8b21a0ed6740fe5f5c3068762469bdc8
kunpaoo <txcokun@gmail.com>
2024-09-25 10:24:16 +0800

    fix: landing page overflow, mobile view on login

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -22,60 +22,65 @@ class MobileWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            child: Column(children: [
-      Image.asset('assets/khun.png',
-          width: 350, // Set the desired width
-          height: 350),
-      const Text("Discover Your ",
-          style: TextStyle(
-            color: Color.fromRGBO(22, 54, 169, 1),
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          )),
-      const Text("Dream Cat Here",
-          style: TextStyle(
-            color: Color.fromRGBO(22, 54, 169, 1),
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          )),
-      const Padding(
-          padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
-          child: const Text(
-            textAlign: TextAlign.center,
-            "Explore all the existing job roles based on your interest and study major",
-          )),
-      SizedBox(height: 35),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        RawMaterialButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/login');
-          },
-          fillColor: Color.fromRGBO(22, 54, 169, 1),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-          padding: EdgeInsets.fromLTRB(45, 12, 45, 12),
-          child: const Text("Login",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16)),
-        ),
-        SizedBox(width: 30),
-        RawMaterialButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/signup');
-          },
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          padding: EdgeInsets.fromLTRB(45, 12, 45, 12),
-          child: const Text("Register",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16)),
-        )
-      ]),
-    ])));
+        body: SingleChildScrollView(
+      child: Container(
+          padding: EdgeInsets.only(bottom: 80),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Image.asset('assets/khun.png', width: 350, height: 350),
+                const Text("Discover Your ",
+                    style: TextStyle(
+                      color: Color.fromRGBO(22, 54, 169, 1),
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    )),
+                const Text("Dream Cat Here",
+                    style: TextStyle(
+                      color: Color.fromRGBO(22, 54, 169, 1),
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    )),
+                const Padding(
+                    padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
+                    child: const Text(
+                      textAlign: TextAlign.center,
+                      "Explore all the existing job roles based on your interest and study major",
+                    )),
+                SizedBox(height: 35),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  RawMaterialButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    fillColor: Color.fromRGBO(22, 54, 169, 1),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    padding: EdgeInsets.fromLTRB(45, 12, 45, 12),
+                    child: const Text("Login",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16)),
+                  ),
+                  SizedBox(width: 30),
+                  RawMaterialButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: EdgeInsets.fromLTRB(45, 12, 45, 12),
+                    child: const Text("Register",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16)),
+                  )
+                ]),
+              ])),
+    ));
   }
 }
 
@@ -83,59 +88,61 @@ class DesktopWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            color: Colors.white,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('khun.png'),
-                SizedBox(width: 250),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Discover Your Dream Cat Here",
-                        style: TextStyle(
-                          color: Color.fromRGBO(22, 54, 169, 1),
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    const Text(
-                        "Explore all the existing job roles based on your interest and study major"),
-                    SizedBox(height: 20),
-                    Row(children: [
-                      RawMaterialButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/login');
-                        },
-                        fillColor: Color.fromRGBO(22, 54, 169, 1),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        padding: EdgeInsets.fromLTRB(60, 15, 60, 15),
-                        child: const Text("Login",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 16)),
-                      ),
-                      SizedBox(width: 50),
-                      RawMaterialButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/signup');
-                        },
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        padding: EdgeInsets.fromLTRB(60, 15, 60, 15),
-                        child: const Text("Register",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 16)),
-                      )
-                    ]),
-                  ],
-                )
-              ],
-            )));
+        body: SingleChildScrollView(
+      child: Container(
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('khun.png'),
+              SizedBox(width: 250),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Discover Your Dream Cat Here",
+                      style: TextStyle(
+                        color: Color.fromRGBO(22, 54, 169, 1),
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  const Text(
+                      "Explore all the existing job roles based on your interest and study major"),
+                  SizedBox(height: 20),
+                  Row(children: [
+                    RawMaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                      fillColor: Color.fromRGBO(22, 54, 169, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      padding: EdgeInsets.fromLTRB(60, 15, 60, 15),
+                      child: const Text("Login",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16)),
+                    ),
+                    SizedBox(width: 50),
+                    RawMaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      padding: EdgeInsets.fromLTRB(60, 15, 60, 15),
+                      child: const Text("Register",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16)),
+                    )
+                  ]),
+                ],
+              )
+            ],
+          )),
+    ));
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -8,14 +8,16 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: ResponsiveBuilder(
-        builder: (context, sizingInformation) {
-          if (sizingInformation.deviceScreenType == DeviceScreenType.mobile) {
-            return MobileWidget();
-          } else {
-            return DesktopWidget();
-          }
-        },
+      body: SingleChildScrollView(
+        child: ResponsiveBuilder(
+          builder: (context, sizingInformation) {
+            if (sizingInformation.deviceScreenType == DeviceScreenType.mobile) {
+              return MobileWidget();
+            } else {
+              return DesktopWidget();
+            }
+          },
+        ),
       ),
     );
   }
@@ -62,73 +64,71 @@ class LoginInterface extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: SingleChildScrollView(
-      child: Container(
-        width: 400,
-        height: 600,
-        padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 50),
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(10)),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Login here",
-              style: TextStyle(
-                  fontSize: 25, fontWeight: FontWeight.w900, color: primary),
+        child: Container(
+      width: 400,
+      height: 600,
+      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 50),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Login here",
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.w900, color: primary),
+          ),
+          const SizedBox(height: 20),
+          const SizedBox(
+            width: 200,
+            child: Text(
+              "Welcome back you've been missed!",
+              softWrap: true,
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 20),
-            const SizedBox(
-              width: 200,
-              child: Text(
-                "Welcome back you've been missed!",
-                softWrap: true,
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
+          ),
+          const SizedBox(height: 20),
+          InputText('Username'),
+          InputText('Password', true),
+          SizedBox(
+            width: double.infinity,
+            child: Text("Forgot your Password?",
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  color: primary,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 10,
+                )),
+          ),
+          SizedBox(height: 20),
+          MaterialButton(
+            onPressed: () {
+              Navigator.popAndPushNamed(context, '/home');
+            },
+            color: primary,
+            minWidth: double.infinity,
+            elevation: 10,
+            padding: EdgeInsets.all(20),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+            child: Text(
+              "Sign in",
+              style: TextStyle(color: Colors.white),
             ),
-            const SizedBox(height: 20),
-            InputText('Username'),
-            InputText('Password', true),
-            SizedBox(
-              width: double.infinity,
-              child: Text("Forgot your Password?",
-                  textAlign: TextAlign.end,
-                  style: TextStyle(
-                    color: primary,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 10,
-                  )),
-            ),
-            SizedBox(height: 20),
-            MaterialButton(
-              onPressed: () {
-                Navigator.popAndPushNamed(context, '/home');
-              },
-              color: primary,
-              minWidth: double.infinity,
-              elevation: 10,
-              padding: EdgeInsets.all(20),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)),
-              child: Text(
-                "Sign in",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            SizedBox(height: 50),
-            Text(
-              "or continue with",
-              style: TextStyle(
-                  color: Color.fromRGBO(31, 65, 187, 1),
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            SocialIconsWidget(),
-          ],
-        ),
+          ),
+          SizedBox(height: 50),
+          Text(
+            "or continue with",
+            style: TextStyle(
+                color: Color.fromRGBO(31, 65, 187, 1),
+                fontSize: 12,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          SocialIconsWidget(),
+        ],
       ),
     ));
   }
